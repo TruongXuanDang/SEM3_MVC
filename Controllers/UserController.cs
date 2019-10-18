854f503e3aa5843a469d69c6f7f9a91a26da60e0
TruongDX8 <truonghusk17@gmail.com>
2019-10-18 23:02:13 +0700

    First form

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using T1807MVC.Models;
+
+namespace T1807MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Store(User user)
+        {
+            var jsonUser = JsonConvert.SerializeObject(user);
+            Debug.WriteLine(jsonUser);
+            ViewBag.user = user;
+            return View();
+        }
+    }
+}
