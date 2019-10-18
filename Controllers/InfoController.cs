55adfe12241cda13198577566626f6f03160c2f5
TruongDX8 <truonghusk17@gmail.com>
2019-10-18 18:11:04 +0700

    First commit

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T1807MVC.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register(string username, string fullname, string password)
+        {
+            ViewBag.Username = username;
+            ViewBag.Fullname = fullname;
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T1807MVC.MvcApplication" Language="C#" %>
